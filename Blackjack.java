72f71e726657c537768cd3523ec5e94110f79a75
reecetyl <31014959+reecetyl@users.noreply.github.com>
2019-09-19 12:21:23 -0400

    Create Blackjack.java

---
+++ Blackjack.java
@@ -0,0 +1,74 @@
+// Blackjack.java
+
+public class Blackjack {
+    public static void main(String args[]) {
+        // main code for blackjack game
+    }
+
+}
+
+// abstract class for Player
+abstract class Player {
+    // static variables
+    // nonstatic variables
+    // constructors
+    // mutators
+    // accessors
+    // prints/compares/etc
+    // static methods
+}
+
+// HumanPlayer is a type of player
+public class HumanPlayer extends Player {
+    // static variables
+    // nonstatic variables
+    // constructors
+    // mutators
+    // accessors
+    // prints/compares/etc
+    // static methods
+}
+
+// Dealer has some of the same methods/fields as Player
+public class Dealer extends Player {
+    // static variables
+    // nonstatic variables
+    // constructors
+    // mutators
+    // accessors
+    // prints/compares/etc
+    // static methods
+}
+
+// class to represent a Deck
+public class Deck {
+    // static variables
+    // nonstatic variables
+    // constructors
+    // mutators
+    // accessors
+    // prints/compares/etc
+    // static methods
+}
+
+// class to represent a Hand of Cards
+public class Hand {
+    // static variables
+    // nonstatic variables
+    // constructors
+    // mutators
+    // accessors
+    // prints/compares/etc
+    // static methods
+}
+
+// class to represent a Card
+public class Card {
+    // static variables
+    // nonstatic variables
+    // constructors
+    // mutators
+    // accessors
+    // prints/compares/etc
+    // static methods
+}

8611c885552bcf0b2fe1a0c2504c4c3c1caa2bd8
Reece <ty29192@502158-mitll.llan.ll.mit.edu>
2019-09-23 08:30:50 -0400

    add Hand class

--- Hand.java
+++ Hand.java
@@ -1,17 +1,37 @@
-// class to represent a Hand of Cards
+import java.util.*;
+
 public class Hand {
-    // static variables
-    // nonstatic variables
-    Card[] cards;
-    // constructors
-    // mutators
-    // accessors
-    // prints/compares/etc
-    String toString() {
+    List<Card> cards = new ArrayList<>();
+    
+    public void add(Card card) {
+	cards.add(card);
+    }
+
+    public void add(List<Card> cards) {
+	this.cards.addAll(cards);
+    }
+
+    public void clear() {
+	cards.clear();
+    }
+ 
+    public List<Card> getCards() {
+	return cards;
+    }
+
+    public Integer getScore() {
+	Integer handScore = 0;
+	for(Card c : cards) {
+		handScore += c.getScore();
+	}
+	return handScore;
+    }
+    
+    public String toString() {
         String cardString = "";
         for(Card c: cards) {
             cardString += c.toString() + " ";
         }
+	return cardString;
     }
-    // static methods
 }

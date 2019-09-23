8aaa37fec9fd5bdc68a85cee535fbf5856fd5f41
Reece <ty29192@502158-mitll.llan.ll.mit.edu>
2019-09-23 08:59:15 -0400

    add some to main class and add move class for calling in main

--- Blackjack.java
+++ Blackjack.java
@@ -1,16 +1,43 @@
-public class Blackjack {
-	
+public class Blackjack {	
+
 	public static void main(String[] args) {
 		Blackjack blackjack = new Blackjack();
 		blackjack.play();
 	}
 
+	Deck deck = new Deck();
+	Player dealer = new Dealer();
+	Player player;
+	
+
 	Blackjack() {
-		// initialize blackjack
+		Scanner userInp = new Scanner(System.in);
+		System.out.println("Welcome to Tyler, Sean, and Gary's CS591 Blackjack Game!")
+		System.out.println("Please enter your name to begin: ");
+		String name = userInp.nextLine();
+		player = new Player(name);
 	}
 
 	public void play() {
-		// stuff for playing blackjack
+		Card hiddenDealerCard = deckOfCards.dealOne();
+			
 	}
 
+	public void giveCard(Player p, Card c) {
+
+	}
+
+	public boolean gameOver() {
+		if (player.totalPoints() >= 21 || dealer.totalPoints() >= 21) return true;
+		else return false;
+	}
+
+	public void showWinner() {
+		if(player.totalPoints() >= 21) System.out.println(player.getName() + " has lost");
+		else if (dealer.totalPoints() >= 21) System.out.println(dealer.getName() + " has lost");
+		else {
+			Player winner = (player.totalPoints() > dealer.totalPoints()) ? player : dealer;
+			System.out.println(winner.getName() + " wins");
+		}
+	}
 }
---
+++ Move.java
@@ -0,0 +1,22 @@
+public class Move {
+	Player player;
+	Card card;
+	
+	public Move(Player p, Card c) {
+		this.player = p;
+		this.card = c;
+	}
+
+	public Player getPlayer() {
+		return this.player;
+	}
+	
+	public Card getCard() {
+		return this.card;
+	}
+
+	public String toString() {
+		return "Move: " + player.getName() + " " + " takes Card " + card.toString() + " for " + String(card.getScore()) + " points";
+	}
+
+}

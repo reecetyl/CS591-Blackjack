d00fa9e55f47919f5d3f78eb8ed0ba2185cf129c
Reece <ty29192@502158-mitll.llan.ll.mit.edu>
2019-09-23 08:35:49 -0400

    add main Blackjack class

---
+++ Blackjack.java
@@ -0,0 +1,16 @@
+public class Blackjack {
+	
+	public static void main(String[] args) {
+		Blackjack blackjack = new Blackjack();
+		blackjack.play();
+	}
+
+	Blackjack() {
+		// initialize blackjack
+	}
+
+	public void play() {
+		// stuff for playing blackjack
+	}
+
+}
